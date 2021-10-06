7fcd54e39a733c1bd41b82199dfef8928324ae41
serhat <serhat.sural@ericssonmsp.com>
2021-10-06 16:59:48 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿namespace GenericsIntro
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _arrayKeys;
+        TValue[] _arrayValues;
+
+        TKey[] _tempArrayKey;
+        TValue[] _tempArrayValue;
+
+        public MyDictionary()
+        {
+            _arrayKeys = new TKey[0];
+            _arrayValues = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            _tempArrayKey = _arrayKeys; //geçici dizi
+            _tempArrayValue = _arrayValues; //geçici dizi
+
+            _arrayKeys = new TKey[_arrayKeys.Length + 1];
+            _arrayValues = new TValue[_arrayValues.Length + 1];
+
+            for (int i = 0; i < _tempArrayKey.Length; i++)
+            {
+                _arrayKeys[i] = _tempArrayKey[i];
+            }
+
+            for (int j = 0; j < _tempArrayValue.Length; j++)
+            {
+                _arrayValues[j] = _tempArrayValue[j];
+
+            }
+
+            _arrayKeys[_arrayKeys.Length - 1] = key;
+            _arrayValues[_arrayValues.Length - 1] = value;
+        }
+
+        public int Length
+        {
+            get { return _arrayKeys.Length; }
+        }
+
+        public TKey[] Key
+        {
+            get { return _arrayKeys; }
+        }
+
+        public TValue[] Values
+        {
+            get { return _arrayValues; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> names = new MyList<string>();
+            //Listeye ekle
+            names.Add("Serhat");
+            names.Add("Sural");
+            //listedeki değerleri oku
+            foreach (var item in names.Items)
+            {
+                Console.WriteLine(item);
+            }
+            //Listenin uzunluğunu göster
+            Console.WriteLine(names.Length);
+
+            MyDictionary<string, int> product = new MyDictionary<string, int>();
+            product.Add("Laptop", 1500);
+            product.Add("Mouse", 500);
+            product.Add("Keyboard", 750);
+
+            foreach (var products in product.Values)
+            {
+                Console.WriteLine(products);
+            }
+
+            foreach (var products in product.Key)
+            {
+                Console.WriteLine(products);
+            }
+
+
+        }
+    }
+}
